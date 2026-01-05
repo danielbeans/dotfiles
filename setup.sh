778e44a4e4c312d6a093da5400e56223740201a3
Daniel Williams <danielfwilliams@proton.me>
2026-01-04 20:43:44 -0500

    Update mise installation

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 # Install mise (if not present)
 curl https://mise.jdx.dev/install.sh | sh
 echo "eval \"\$(~/.local/bin/mise activate bash)\"" >> ~/.bashrc
+exec /bin/bash
 
 # Install chezmoi
 mise use --global chezmoi@latest
