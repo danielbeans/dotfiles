def7deb3bfd7c4b65e51d1f6e4dfedad475f5e04
Daniel Williams <danielfwilliams@proton.me>
2026-01-04 22:20:42 -0500

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -4,5 +4,9 @@ winget install jdx.mise
 # Install Chezmoi dotfile manager
 winget install twpayne.chezmoi
 
+# Refresh environment path
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+Write-Host "Environment Path refreshed."
+
 # Init dotfiles repo
 chezmoi init --apply https://github.com/danielbeans/dotfiles.git
