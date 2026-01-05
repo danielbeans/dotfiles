13789cc289ab100fa61055fbbe2155ac1f170080
Daniel Williams <72041087+danielbeans@users.noreply.github.com>
2026-01-05 11:23:44 -0500

    Update chezmoi init command to use SSH URL

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ curl https://mise.jdx.dev/install.sh | sh
 $MISE_BIN use --global chezmoi@latest
 
 # Initialize chezmoi
-$MISE_BIN exec -- chezmoi init --apply https://github.com/danielbeans/dotfiles.git
+$MISE_BIN exec -- chezmoi init --apply git@github.com:danielbeans/dotfiles.git
 
 # Run zsh
 echo "eval \"\$(~/.local/bin/mise activate zsh)\"" >> ~/.zshrc
